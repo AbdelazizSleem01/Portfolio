f0a5f5ac6bd773792ee465b1284d44639b28d5ec
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2025-08-14 01:25:49 +0300

    updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+// postcss.config.js
+export default {
   plugins: {
-    tailwindcss: {},
-  },
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}
+  }
 };
-
-export default config;
